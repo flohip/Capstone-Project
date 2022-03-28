953c01bc848b1edebba4f48539d835e738003728
Florian Hippenstiel <florian.hippenstiel@web.de>
2022-03-28 17:27:36 +0200

    installed storybook

---
+++ components/RequestedWord/RequestedWord.js
@@ -0,0 +1,6 @@
+const RequestedWord = () => {
+  return <div>RequestedWord</div>;
+};
+export default RequestedWord;
+
+const StyledDiv = styled.div``;
---
+++ components/RequestedWord/RequestedWord.stories.js
@@ -0,0 +1,19 @@
+// import DefaultHtml from "./Header.default.html";
+
+export default {
+  title: "Components/RequestedWord",
+};
+
+// export const Default = () => DefaultHtml;
+
+export const RequestedWord = () =>
+  /*react*/
+  `
+      <div class="Stories">
+      <div class="Stories__grid Stories__grid1">
+        <div class="Stories__photo">
+          <img src="/media/Stories/Story 1.svg" alt="" class="Stories__img Stories__img1" />
+        </div>
+      </div>
+    
+    `;
