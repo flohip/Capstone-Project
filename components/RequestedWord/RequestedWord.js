fedc97817f0baa14bb9c3bc7f568be30af14cde1
Florian Hippenstiel <florian.hippenstiel@web.de>
2022-03-30 14:57:24 +0200

    added media queries for font-size

--- components/RequestedWord/RequestedWord.js
+++ components/RequestedWord/RequestedWord.js
@@ -22,18 +22,20 @@ const Container = styled.ul`
   width: fit-content;
   max-width: 80vw;
   padding: 0.3rem;
-  background-color: rgb(222, 222, 222);
   color: white;
   display: flex;
   flex-wrap: nowrap;
   gap: 1rem;
 `;
 const StyledLi = styled.li`
+  font-size: inherit;
+  font-size: calc(inherit * 2);
   width: fit-content;
-  max-width: 80vw;
-  padding: 1rem;
+  padding: 0.3rem;
   gap: 0.3rem;
-  background-color: steelblue;
-  color: white;
+  color: var(--fontColor);
   list-style-type: none;
+  > li:hover {
+    border: 0.3rem solid green;
+  }
 `;
