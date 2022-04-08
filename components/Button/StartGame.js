dcd89c5f4b2fffcebbfe77c5b9c23cd9b8da9c0c
Florian Hippenstiel <florian.hippenstiel@web.de>
2022-04-08 12:26:48 +0200

    added a next/image of gummi bears

--- components/Button/StartGame.js
+++ components/Button/StartGame.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import gummiBearPicture from "../../images/altair-valasek-6uHnajYSsW0-unsplash.jpg";
 import styled from "styled-components";
 export default function StartGame({ setGameStarted, wonGame, setWonGame }) {
   function startTheGame() {
@@ -10,6 +12,12 @@ export default function StartGame({ setGameStarted, wonGame, setWonGame }) {
         Du hast das Wort erraten!!! Hier sind ein paar Gummibärchen als
         Belohnung...
       </StyledHeader>
+      <Image
+        src={gummiBearPicture}
+        alt="a picture of gummi bears"
+        width="200"
+        height="92"
+      />
       <StyledStartButton onClick={startTheGame}>
         Nächstes Spiel
       </StyledStartButton>
@@ -28,11 +36,12 @@ export default function StartGame({ setGameStarted, wonGame, setWonGame }) {
 }
 const StyledStartButton = styled.button`
   margin: 2rem;
+  padding: 0.3rem;
   width: 50vw;
-  height: 10vw;
+  min-height: 10vw;
+  height: fit-content;
   border-radius: 10px;
   font-size: 2rem;
-
   background-color: rgb(0, 170, 0);
 `;
 const StyledHeader = styled.h1`
@@ -41,4 +50,8 @@ const StyledHeader = styled.h1`
   font-size: 2rem;
   color: var(--fontColor);
   text-shadow: -1px 0 black;
+`;
+
+const StyledImage = styled.img`
+  color: red;
 `;
